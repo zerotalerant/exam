6bbebc5584de0bdda7647e259202d3580a127f44
zerotalerant <hasa32892@gmail.com>
2022-01-23 19:09:19 +0600

    2 Задача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,10 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите набор символов: ");
-        String pldrm = scanner.nextLine();
-        boolean palindrom = true;
-        for (int i = 0; i <= pldrm.length() - 1; i++) {
-            for (int j = pldrm.length() - 1; j >= 0; j--) {
-                if (pldrm.charAt(i) != pldrm.charAt(j)) {
-                    palindrom = false;
-                } else {
-                    palindrom = true;
-                }
-            }
+        char[] array = {'П', 'р', 'о', 'г', 'р', 'а', 'м', 'м', 'и', 'с', 'т'};
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
         }
-        System.out.println(palindrom);
     }
 }
