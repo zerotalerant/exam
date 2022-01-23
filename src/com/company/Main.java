1d971e0979328015539467ee3cd78621b158ba1c
zerotalerant <hasa32892@gmail.com>
2022-01-23 19:17:27 +0600

    1 Задача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,67 @@ package com.company;
 
 import java.util.Scanner;
 
+import static com.company.Main.Test.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите набор символов: ");
-        String pldrm = scanner.nextLine();
-        boolean palindrom = true;
-        for (int i = 0; i <= pldrm.length() - 1; i++) {
-            for (int j = pldrm.length() - 1; j >= 0; j--) {
-                if (pldrm.charAt(i) != pldrm.charAt(j)) {
-                    palindrom = false;
-                } else {
-                    palindrom = true;
-                }
+        System.out.print("Введите число: ");
+        int p = scanner.nextInt();
+        System.out.print("Введите степень: ");
+        double n = scanner.nextDouble();
+        System.out.println("Ответ: " + power(p, n));
+        System.out.println("Shor: " + aShortPower(p, n));
+        System.out.println("Int: " + anIntPower(p, n));
+        System.out.println("Long: " + aLongPower(p, n));
+        System.out.println("Float: " + aFloatPower(p, n));
+    }
+    public class Test {
+        static double power(int p, double n) {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = result * p;
             }
+            return result;
         }
-        System.out.println(palindrom);
+
+        @Overload
+        static short aShortPower(int p, double n) {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = result * p;
+            }
+            return (short) result;
+        }
+
+        @Overload
+        static int anIntPower(int p, double n) {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = result * p;
+            }
+            return result;
+        }
+
+        @Overload
+        static long aLongPower(int p, double n) {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = result * p;
+            }
+            return result;
+        }
+
+        @Overload
+        static float aFloatPower(int p, double n) {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = result * p;
+            }
+            return result;
+        }
     }
 }
+
+
+
